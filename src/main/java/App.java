be858b106dca9f057ed98b65cc3d76f7975d4eeb
MihailKrasheninnikov <rolerlozz@gmail.com>
2024-02-12 06:30:45 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat Catbean =
+                (Cat) applicationContext.getBean("cat");
+        Cat Catbean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println("HelloWorld " + (bean == bean2));
+        System.out.println("Cat " + (Catbean == Catbean2));
+
     }
 }
